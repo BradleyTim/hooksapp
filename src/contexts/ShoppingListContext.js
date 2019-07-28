d4fe345ecdc782acfc667e0b1dd11bb84f1eba24
BradleyTim <timbradley94@gmail.com>
2019-07-28 13:49:24 +0300

    add localStoarage to persist data

--- src/contexts/ShoppingListContext.js
+++ src/contexts/ShoppingListContext.js
@@ -1,10 +1,17 @@
-import React, { useReducer, createContext } from "react";
+import React, { useReducer, createContext, useEffect } from "react";
 import shoppingReducer from "../reducers/ShoppingReducer";
 
 export const ShoppingListContext = createContext();
 
 const ShoppingListContextProvider = props => {
-  const [shopping, dispatch] = useReducer(shoppingReducer, []);
+  const [shopping, dispatch] = useReducer(shoppingReducer, [], () => {
+    const shoppingData = localStorage.getItem("shopping");
+    return shoppingData ? JSON.parse(shoppingData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("shopping", JSON.stringify(shopping));
+  }, [shopping]);
 
   return (
     <ShoppingListContext.Provider value={{ shopping, dispatch }}>
